fc07bdec6f99657ac8509d4fa839b9df11ffadd8
Manel <ea3eiz@ea3eiz.com>
2020-12-29 14:07:13 +0100

    1

--- editar_MMDVMDMR2M17.sh
+++ editar_MMDVMDMR2M17.sh
@@ -1104,13 +1104,13 @@ done;;
 h) echo ""
 while true
 do
-                          actualizar=S 
-                          case $actualizar in
-			                    [sS]* ) echo ""
-                          sh Puertos_Nextion_DMR2YSF.sh
-			                    break;;
-			                    [nN]* ) echo ""
-			                    break;;
+                        actualizar=S 
+                        case $actualizar in
+			      [sS]* ) echo ""
+                        sh Puertos_Nextion_DMR2M17.sh
+			      break;;
+			      [nN]* ) echo ""
+			      break;;
 esac
 done;;
 i) echo ""
