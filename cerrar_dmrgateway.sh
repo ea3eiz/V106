27f19dea9a936a46bc346e7a16f554b65f2d5a19
Manel <ea3eiz@ea3eiz.com>
2020-12-30 19:54:18 +0100

    1

--- cerrar_dmrgateway.sh
+++ cerrar_dmrgateway.sh
@@ -6,7 +6,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_dmrgateway.desktop /home/pi/
 sleep 1
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=72x20 -e sudo sh ejecutar_dmrgateway.sh'" /home/pi/Abrir_dmrgateway.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=77x18 -e sudo sh ejecutar_dmrgateway.sh'" /home/pi/Abrir_dmrgateway.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRGATEWAY_OFF.png" /home/pi/Abrir_dmrgateway.desktop
 sed -i "10c Name[es_ES]=Abrir DMRGateway" /home/pi/Abrir_dmrgateway.desktop
 sed -i "19c DMRGateway=OFF" /home/pi/.local/status.ini
