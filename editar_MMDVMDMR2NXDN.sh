f4ac377ebfd76f1932152ec9379496f47df7c37a
Manel <ea3eiz@ea3eiz.com>
2021-01-07 00:26:23 +0100

    1

--- editar_MMDVMDMR2NXDN.sh
+++ editar_MMDVMDMR2NXDN.sh
@@ -352,7 +352,7 @@ numero_linea_local=`expr $numero_linea + 7` # y le suma uno qudando coomo: (143)
 letra=p
 numero_linea_p=$numero_linea_local$letra #crea 196p
 echo -n "\33[1;36m  29)\33[0m Local port            - ${VERDE}"
-presentar_valor= sed -n $numero_linea_p  $usuario/MMDVMHost/$DIRECTORIO; #presenta el valor en pantalla
+presentar_valor_local= sed -n $numero_linea_p  $usuario/MMDVMHost/$DIRECTORIO; #presenta el valor en pantalla
 echo ""
 
 echo ""
@@ -1308,7 +1308,7 @@ done;;
 29) echo ""
 while true
 do
-                          echo  "   Valor actual $presentar_valor${AMARILLO}"
+                          echo  "   Valor actual $presentar_valor_local${AMARILLO}"
                           read -p 'Introducir el puerto: 62032  '   dmrac1
                           actualizar=S 
                           case $actualizar in
