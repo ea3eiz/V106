e58f2692c03700c7d2a7ea6c64898075f5f06d69
Manel <ea3eiz@ea3eiz.com>
2021-01-15 18:00:21 +0100

    1

--- Configuracion_especial_red.sh
+++ Configuracion_especial_red.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-while true
-do
 clear
 usuario="/home/pi"
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
@@ -15,37 +12,6 @@ AMARILLO="\033[1;33m"
 CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
-echo "${VERDE}"
-echo "   ************************************************************************"
-echo -n "${CIAN}"
-echo "                      Script para Configuración de red    "
-echo -n "${ROJO}"
-echo "                              $SCRIPTS_version by EA3EIZ"
-echo -n "${VERDE}"
-echo "   ************************************************************************"
-#echo "\33[1;36m   1)\33[1;37m Actualizar imagen" 
-echo "\33[1;36m   1)${BLANCO} Configurar ip fija por cable"
-echo ""
-echo "\33[1;36m   ${AMARILLO}   Para configurar ip fija por Wifi se debe de utilizar el icono"
-echo "      superior derecho y elegir Editar conexiones...."
-echo ""
-
-echo ""
-echo "   ${ROJO}0) Salir"
-echo ""
-echo -n "\33[1;36m   Elige una opción: " 
-read escoger_menu
-echo ""
-case $escoger_menu in
-1) echo ""
-while true
-do
-clear                     
-                        instalarsi=S
-                        case $instalarsi in
-                        [sS]* ) echo ""
-                        clear
-
                         echo "${VERDE}"
                         echo "   ************************************************************************" 
                         echo "                   SCRIPT PARA ESTABLECER IP FIJA POR CABLE               "
@@ -112,24 +78,4 @@ clear
                         echo "   ************************************************************************"
                         sleep 5
                         fi
-                        break;;
-                        [nN]* ) echo ""
-                        clear
-                        exit;
-                        break;;
-esac
-done;;
-0) 
-clear
-echo "\v\v\v\v\v\v\v\v\v\v\v"
-echo "${AMARILLO}"
-echo "   ************************************************************************"
-echo "   *                                                                      *"
-echo "   *                          CERRANDO SCRIPT                             *"
-echo "   *                                                                      *"
-echo "   ************************************************************************"
-sleep 2
-clear
-exit;;	
-esac
-done
+                    
