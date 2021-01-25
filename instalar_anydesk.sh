a5ceddb69a468eb4f8ae32fee32e5be6c82510e5
Manel <ea3eiz@ea3eiz.com>
2021-01-26 00:17:26 +0100

    1

--- instalar_anydesk.sh
+++ instalar_anydesk.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 clear
-
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+MARRON="\33[38;5;138m"
                         # Comprueba si Anydesk está instalado
                         estado_anydesk=$(awk "NR==12" /home/pi/info.ini)
                         if [ "$estado_anydesk" = 'ANYDESK=ON' ];then
