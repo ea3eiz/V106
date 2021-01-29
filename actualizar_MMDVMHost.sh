ffe479a3b1d91b61806ad90b51fa39ff32c05bb2
Manel <ea3eiz@ea3eiz.com>
2021-01-29 16:54:05 +0100

    1

--- actualizar_MMDVMHost.sh
+++ actualizar_MMDVMHost.sh
@@ -1,37 +1,4 @@
 ﻿#!/bin/bash
-#/home/pi/V105/./qt_mmdvmhost_off
-#exit;
-clear
-SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-#Colores
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-CIAN="\033[1;36m"
-GRIS="\033[0m"
-MARRON="\33[38;5;138m"
-
-                    echo "${ROJO}"
-                    echo " *********************************************************************"
-                    echo " * Si actualizas MMDVMhost desde esta opción, perderás todas las     *"
-                    echo " * configuraciones y tendrás que volver a configurar los .ini        *"
-                    echo " *********************************************************************"
-                    
-                    echo "${BLANCO}"
-                    echo " *********************************************************************"
-                    echo " * Haz antes una copia de seguridad desde el icono COPIAR RESTAURAR   *"
-                    echo " * O desde un navegador poniendo la ip de la raspberry pi y luego     *"
-                    echo " * utiLiza el icono HACER COPIA SEGURIDAD el cual te creará           *"
-                    echo " * el archivo [copia.tar.gz] en la carpeta Downloads de tu ordenador  *"
-                    echo " *********************************************************************"                    
-
-                    echo "${VERDE}"
-                    read -p '  Quieres hacer la actualización? S/N: ' ejecutar1
-                    case $ejecutar1 in
-                    [sS]* ) echo ""
-                    echo "ok >>>>>"
-
                     # guarda el fichero MMDVMDMRGateway.ini para recuperarlo después de la actualización
                     cp /home/pi/MMDVMHost/MMDVMDMRGateway.ini /home/pi
                     cd /home/pi
@@ -54,15 +21,6 @@ MARRON="\33[38;5;138m"
                     PUNTO=";"   
                     
                     sed -i "22c $FIJA$HOY1$PUNTO" /home/pi/MMDVMHost/Version.h
-
-
-
-# cp /home/pi/$SCRIPTS_version/YSFControl.cpp /home/pi/MMDVMHost
-# cp /home/pi/$SCRIPTS_version/MMDVMHost.cpp /home/pi/MMDVMHost
-# cp /home/pi/$SCRIPTS_version/Conf.cpp /home/pi/MMDVMHost
-# cp /home/pi/$SCRIPTS_version/Conf.h /home/pi/MMDVMHost
-# cp /home/pi/$SCRIPTS_version/YSFControl.h /home/pi/MMDVMHost
-
 
                     make clean
                     make -f Makefile.Pi.OLED
@@ -139,17 +97,4 @@ MARRON="\33[38;5;138m"
                     #=================================================
 
                     sudo chmod 777 -R /home/pi/MMDVMHost
-                    
-                    echo""
-                    clear
-                    exit;
-                    echo ""
-                    echo "Ok, se ha ejecutado correctamente"
-                    echo ""
-                    ;;
-                    [nN]* ) echo ""
-                    ;;
-                    esac
-                    sleep 1
-                    clear
                     exit;
