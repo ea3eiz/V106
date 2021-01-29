f1cab1f3392bebec5358f1cffce3b6382a7fc03e
Manel <ea3eiz@ea3eiz.com>
2021-01-29 18:37:02 +0100

    1

---
+++ actualizar_listado_nxdn.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+
+# path usuario
+usuario=$(awk "NR==1" /home/pi/.config/autostart/usuario)
+
+SCRIPTS_version=$(awk "NR==1" $usuario/.config/autostart/version)                                               
+actualizar=S 
+case $actualizar in
+[sS]* ) echo ""                        
+cd $usuario/NXDNClients/NXDNGateway/
+rm -R private
+mkdir private
+cd $usuario/$SCRIPTS_version
+cp NXDNHosts.txt $usuario/NXDNClients/NXDNGateway/private
+cp NXDNHosts.txt $usuario/NXDNClients/NXDNGateway                            
+exit	
+
