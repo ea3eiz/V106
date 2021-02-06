edb9759e2c31f04eb13b65c4d59299c39f7bcb64
Manel <ea3eiz@ea3eiz.com>
2021-02-07 00:52:20 +0100

    1

--- ejecutar_dmrgateway.sh
+++ ejecutar_dmrgateway.sh
@@ -18,7 +18,7 @@ sudo cp RXF_DMRGATEWAY.desktop /home/pi
 frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMDMRGATEWAY.ini)
 frecuencia=`expr substr $frecuencia 13 17`
 frecuencia=$frecuencia$puerto
-sed -i "12c Name=$frecuencia" /home/pi/RXF_DMRGATEWAY.desktop
+sed -i "11c Name=$frecuencia" /home/pi/RXF_DMRGATEWAY.desktop
 
 sudo cp /home/pi/RXF_DMRGATEWAY.desktop /home/pi/Desktop
 
