a79dbcd63b109e8943c020b82ef19374745d3121
Manel <ea3eiz@ea3eiz.com>
2021-02-06 19:37:33 +0100

    1

--- actualizar_MMDVMHost.sh
+++ actualizar_MMDVMHost.sh
@@ -1,7 +1,7 @@
 ﻿#!/bin/bash
                     # guarda el fichero MMDVMDMRGateway.ini para recuperarlo después de la actualización
                     
-                    #cp /home/pi/MMDVMHost/MMDVMDMRGateway.ini /home/pi
+                    cp /home/pi/MMDVMHost/MMDVMDMRGateway.ini /home/pi
                     
                     cd /home/pi
                     sudo rm -r /home/pi/MMDVMHost
@@ -93,10 +93,10 @@
 
                     cp MMDVM.ini MMDVMDMR2M17.ini
 
-                    cp MMDVM.ini MMDVMDMRGateway.ini 
+                    #cp MMDVM.ini MMDVMDMRGateway.ini 
 
                     # recoge el fichero MMDVMDMRGateway.ini para ponerlo en el MMDVMHost
-                    # cp /home/pi/MMDVMDMRGateway.ini /home/pi/MMDVMHost
+                     cp /home/pi/MMDVMDMRGateway.ini /home/pi/MMDVMHost
 
                     #=================================================
 
