774a4894a997681742fb01c45c5cb4cae17bb5d2
Manel <ea3eiz@ea3eiz.com>
2021-02-07 01:10:40 +0100

    1

--- ejecutar_dmrgateway.sh
+++ ejecutar_dmrgateway.sh
@@ -4,18 +4,18 @@
 
 
 
-mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMDMRGATEWAY.ini`
+mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMDMRGateway.ini`
 buscar=":"
 caracteres=`expr index $mode $buscar`
 caracteres_linea=`expr $caracteres - 1`
 numero_linea_port=`expr substr $mode 1 $caracteres_linea`
-mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMDMRGATEWAY.ini)
+mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMDMRGateway.ini)
 puerto=`expr substr $mode 11 9`
 puerto="  "$puerto
 cd /home/pi/Desktop
 sudo cp RXF_DMRGATEWAY.desktop /home/pi
 
-frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMDMRGATEWAY.ini)
+frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMDMRGateway.ini)
 frecuencia=`expr substr $frecuencia 13 17`
 frecuencia=$frecuencia$puerto
 sed -i "11c Name=$frecuencia" /home/pi/RXF_DMRGATEWAY.desktop
