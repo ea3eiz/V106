ff67698ecd22657cc55d6a97b3e871a00e1fbca9
Manel <ea3eiz@ea3eiz.com>
2021-02-12 18:20:55 +0100

    1

--- colocar_iconos.sh
+++ colocar_iconos.sh
@@ -7,7 +7,7 @@ cp /home/pi/icons.screen0-1904x1023.rc /home/pi/.config/xfce4/desktop
 xfdesktop --reload
 
 cp /home/pi/*.desktop /home/pi/Desktop
-rm /home/pi/*.desktop
+sudo rm /home/pi/*.desktop
 sudo chmod 777 -R /home/pi/Desktop
 
 
