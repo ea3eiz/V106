cabfeb46d4f64689d9a237f177f56b1ca088b4bc
Manel <ea3eiz@ea3eiz.com>
2021-02-18 23:18:50 +0100

    1

--- actualizar.sh
+++ actualizar.sh
@@ -37,14 +37,19 @@ sed -i "19c DMRGateway=OFF" $usuario/status.ini
 cd $usuario/$SCRIPTS_version
 git pull
 sleep 2
+
 #Actualiza todos los iconos y Quita todos los iconos verdes que se quedan al cerrar la imagen
-sudo cp $usuario/Desktop/Activar_dvswitch.desktop $usuario/.local #deja el icono en el estado que se reinició
-sudo cp $usuario/Desktop/Activar_NextionDriver.desktop $usuario/.local #deja el icono en el estado que se reinició
+#sudo cp $usuario/Desktop/Activar_dvswitch.desktop $usuario/.local #deja el icono en el estado que se reinició
+#sudo cp $usuario/Desktop/Activar_NextionDriver.desktop $usuario/.local #deja el icono en el estado que se reinició
+
 cd $usuario/$SCRIPTS_version/Desktop
 cp * $usuario/Desktop
 sudo chmod 777 -R $usuario/Desktop
-sudo cp $usuario/.local/Activar_dvswitch.desktop $usuario/Desktop #deja el icono en el estado que se reinició
-sudo cp $usuario/.local/Activar_NextionDriver.desktop $usuario/Desktop #deja el icono en el estado que se reinició
+
+#sudo cp $usuario/.local/Activar_dvswitch.desktop $usuario/Desktop #deja el icono en el estado que se reinició
+#sudo cp $usuario/.local/Activar_NextionDriver.desktop $usuario/Desktop #deja el icono en el estado que se reinició
+
+
 #pone todos los datos de DMR+ , Brandameiter, svxlink etc en panel_control.ini
 bm=`sed -n '2p'  $usuario/MMDVMHost/MMDVMBM.ini`
 plus=`sed -n '2p'  $usuario/MMDVMHost/MMDVMPLUS.ini`
