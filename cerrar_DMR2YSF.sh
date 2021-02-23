8f9f040d61f31fb2abcca6093af277a9a7031d99
Manel <ea3eiz@ea3eiz.com>
2021-02-23 11:35:47 +0100

    1

--- cerrar_DMR2YSF.sh
+++ cerrar_DMR2YSF.sh
@@ -11,27 +11,13 @@ CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
 
-marca_dvswitch=$(awk "NR==20" /home/pi/status.ini)
-if [ "$marca_dvswitch" = 'MARCA_DVSWITCH=ON' ];then
 
-echo "${BLANCO}"
-echo "                  ******************************************"
-echo "                  *           Al cerrar DMR2YSF            *"
-echo "                  *          ${VERDE}se Activa DVSWITCH ${BLANCO}           *"
-echo "                  ******************************************"
-sleep 3
-
-# Activa DVSWITCH 
-cd /home/pi/V106
-sh ejecutar_dvswitch.sh
-
-else
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_DMR2YSF.desktop /home/pi
 
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=77x15 -e sudo sh ejecutar_DMR2YSF.sh'" /home/pi/Abrir_DMR2YSF.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=77x118 --title=DMR2YSF -e sudo sh ejecutar_DMR2YSF.sh'" /home/pi/Abrir_DMR2YSF.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMR2YSF_OFF.png" /home/pi/Abrir_DMR2YSF.desktop
 sed -i "10c Name[es_ES]=Abrir DMR2YSF" /home/pi/Abrir_DMR2YSF.desktop
 sed -i "15c DMR2YSF=OFF" /home/pi/status.ini
@@ -40,4 +26,3 @@ cd /home/pi
 sudo cp Abrir_DMR2YSF.desktop /home/pi/Desktop
 
 sudo rm /home/pi/Abrir_DMR2YSF.desktop
-fi
