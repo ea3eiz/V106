9ba42fd979b8f94eccdf37b7fa751127aa84eb64
Manel <ea3eiz@ea3eiz.com>
2021-02-23 20:36:31 +0100

    1

--- ejecutar_DMR2NXDN.sh
+++ ejecutar_DMR2NXDN.sh
@@ -124,7 +124,8 @@ cd /home/pi/DMR2NXDN
 
 sudo lxterminal --geometry=75x12 -e ./DMR2NXDN DMR2NXDN.ini & 
 cd /home/pi/MMDVMHost
-/home/pi/V106/./qt_info_dmr2nxdn & sudo lxterminal --geometry=75x12 -e ./DMR2NXDN MMDVMDMR2NXDN.ini &
+#/home/pi/V106/./qt_info_dmr2nxdn & sudo lxterminal --geometry=75x12 -e ./DMR2NXDN MMDVMDMR2NXDN.ini &
+sudo lxterminal --geometry=75x12 -e ./DMR2NXDN MMDVMDMR2NXDN.ini &
 cd /home/pi/NXDNClients/NXDNGateway
 sudo ./NXDNGateway NXDNGateway.ini
 
