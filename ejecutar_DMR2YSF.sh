bb6ccb8329878d1a3212f098325d8c06f850abe6
Manel <ea3eiz@ea3eiz.com>
2021-02-23 18:29:30 +0100

    1

--- ejecutar_DMR2YSF.sh
+++ ejecutar_DMR2YSF.sh
@@ -46,7 +46,7 @@ sed -i "14c $frecuencia" /home/pi/INFO_RXF
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_DMR2YSF.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=77x18 -e sudo sh cerrar_DMR2YSF.sh'" /home/pi/Abrir_DMR2YSF.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_DMR2YSF.sh'" /home/pi/Abrir_DMR2YSF.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMR2YSF_ON.png" /home/pi/Abrir_DMR2YSF.desktop
 sed -i "10c Name[es_ES]=Cerrar DMR2YSF" /home/pi/Abrir_DMR2YSF.desktop
 sed -i "15c DMR2YSF=ON" /home/pi/status.ini
