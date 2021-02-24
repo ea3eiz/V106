76e1019eeb8c628ccdc7adcb08290820f7639dc9
Manel <ea3eiz@ea3eiz.com>
2021-02-24 17:48:24 +0100

    1

--- bienvenida.sh
+++ bienvenida.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 #Colores
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-CIAN="\033[1;36m"
-GRIS="\033[0m"
-MARRON="\33[38;5;138m"
+./qt_bienvenida
 
-echo "${VERDE}"
-echo "\v\v\v\v\v"
 
-echo "   *************************************************************************"
-echo -n "${BLANCO}"
-echo "   *************************************************************************"
-echo -n "${AMARILLO}"
-echo "                     BIENVENIDO A LA IMAGEN ADER V106"
-echo "                   MULTIPROTOCOLO PARA  RADIOAFICIONADOS"
-echo -n "${BLANCO}"
-echo "   *************************************************************************"
-echo -n "${VERDE}"
-echo "   *************************************************************************"
-sleep 8
+
+#ROJO="\033[1;31m"
+#VERDE="\033[1;32m"
+#BLANCO="\033[1;37m"
+#AMARILLO="\033[1;33m"
+#CIAN="\033[1;36m"
+#GRIS="\033[0m"
+#MARRON="\33[38;5;138m"
+#
+#echo "${VERDE}"
+#echo "\v\v\v\v\v"
+#
+#echo "   *************************************************************************"
+#echo -n "${BLANCO}"
+#echo "   *************************************************************************"
+#echo -n "${AMARILLO}"
+#echo "                     BIENVENIDO A LA IMAGEN ADER V106"
+#echo "                   MULTIPROTOCOLO PARA  RADIOAFICIONADOS"
+#echo -n "${BLANCO}"
+#echo "   *************************************************************************"
+#echo -n "${VERDE}"
+#echo "   *************************************************************************"
+#sleep 8
