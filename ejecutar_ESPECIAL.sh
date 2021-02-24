0e8eb3d4b3fc1a976d9ae44cead3f41fdfb65400
Manel <ea3eiz@ea3eiz.com>
2021-02-24 11:31:06 +0100

    1

--- ejecutar_ESPECIAL.sh
+++ ejecutar_ESPECIAL.sh
@@ -34,7 +34,7 @@ sed -i "3c $frecuencia" /home/pi/INFO_RXF
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_ESPECIAL.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=77x18 -e sudo sh cerrar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_ESPECIAL_ON.png" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "10c Name[es_ES]=Cerrar Especial" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "10c MMDVMESPECIAL=ON" /home/pi/status.ini
@@ -54,7 +54,7 @@ sleep 2
 sudo ./MMDVMESPECIAL MMDVMESPECIAL.ini
 cd /home/pi/Desktop
 sudo cp Abrir_ESPECIAL.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=77x18 -e sudo sh ejecutar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=77x18 --title=ESPECIAL -e sudo sh ejecutar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_ESPECIAL_OFF.png" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "10c Name[es_ES]=Abrir Especial" /home/pi/Abrir_ESPECIAL.desktop
 
