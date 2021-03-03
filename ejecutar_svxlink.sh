4def6b226aa39a9a84eb9751b10988026576a3c2
Manel <ea3eiz@ea3eiz.com>
2021-03-03 17:12:58 +0100

    1

--- ejecutar_svxlink.sh
+++ ejecutar_svxlink.sh
@@ -33,7 +33,7 @@ sudo svxlink
 
 cd /home/pi/Desktop
 sudo cp Abrir_svxlink.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=50x44 --title=SVXLINK -e sudo sh ejecutar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=50x42 --title=SVXLINK -e sudo sh ejecutar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_SVXLINK_OFF.png" /home/pi/Abrir_svxlink.desktop
 sed -i "10c Name[es_ES]=Abrir SVXLINK" /home/pi/Abrir_svxlink.desktop
 sed -i "8c SVXLINK=OFF" /home/pi/status.ini
