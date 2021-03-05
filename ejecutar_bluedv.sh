fed75a0d0075c7520113b7d094facfb5fa1cb83e
Manel <ea3eiz@ea3eiz.com>
2021-03-05 22:55:09 +0100

    1

--- ejecutar_bluedv.sh
+++ ejecutar_bluedv.sh
@@ -15,7 +15,7 @@ sudo rm /home/pi/Abrir_BlueDV.desktop
 #zenity --info --ok-label= --title=Abriendo_BlueDB --window-icon=/home/pi/V106/ICONO_BLUEDV_ON.png --width=400 --timeout=125 --text "El BlueDv tardará unos minutos en abrirse \ndependiendo de la velocidad de su Raspberry\nsea paciente y espere que se abra."
 
 cd /home/pi/bluedv/
-/home/pi/V106/./aviso_bluedv | mono BlueDV.exe
+/home/pi/V106/./qt_aviso_abriendo_bluedv | mono BlueDV.exe
 
 #mono BlueDV.exe
 
