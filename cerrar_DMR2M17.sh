3236d7405587058daa30c3bbd92f09bf4dd1e01e
Manel <ea3eiz@ea3eiz.com>
2021-03-19 11:38:23 +0100

    1

--- cerrar_DMR2M17.sh
+++ cerrar_DMR2M17.sh
@@ -5,7 +5,7 @@ cp ABRIR_DMR2M17.desktop /home/pi
 sed -i "6c Exec=sh -c 'cd /home/pi/V106; sudo sh ejecutar_DMR2M17.sh'" /home/pi/ABRIR_DMR2M17.desktop
 sed -i "7c Icon=/home/pi/V106/ICONO_M17_OFF.png" /home/pi/ABRIR_DMR2M17.desktop
 sed -i "4c Name[es_ES]=Abrir DMR2M17" /home/pi/ABRIR_DMR2M17.desktop
-#sed -i "7c MMDVMBM=ON" /home/pi/status.ini
+sed -i "21c DMR2M17=OFF" /home/pi/status.ini
 cd /home/pi
 cp ABRIR_DMR2M17.desktop /home/pi/Desktop
 
