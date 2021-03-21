f3d8727bb47b773cd6e0c69b871fd146cecc1cba
Manel <ea3eiz@ea3eiz.com>
2021-03-21 12:10:29 +0100

    1

--- instalar_proxy.sh
+++ instalar_proxy.sh
@@ -5,4 +5,4 @@ cd PROXY
 wget http://www.echolink.org/downloads/EchoLinkProxy_1_2_3.zip
 unzip EchoLinkProxy_1_2_3.zip
 sudo sed -i "23c /home/pi" /etc/rc.local
-sudo sed -i "24c java -jar EchoLinkProxy.jar ELProxy.conf & "/etc/rc.local
+sudo sed -i "24c java -jar EchoLinkProxy.jar ELProxy.conf &" /etc/rc.local
