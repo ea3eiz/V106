e05ea3687b7f071f94e2e2a3198bc4ee9cdb50ed
Manel <ea3eiz@ea3eiz.com>
2021-04-02 17:55:34 +0200

    1

--- instalar_hblink.sh
+++ instalar_hblink.sh
@@ -1,141 +1,76 @@
 #!/bin/bash
-
-apt update
-
-sleep 2
-apt install python3-distutils
+sudo apt update
+sudo apt install python3-distutils
 
 cd /opt/
 wget https://bootstrap.pypa.io/get-pip.py
-sleep 2
-
-python3 get-pip.py
-sleep 2
-apt install python3-twisted
-
-sleep 2
-apt install python3-bitarray
-
-sleep 2
-apt install python3-dev
-
-sleep 2
+sudo python3 get-pip.py
+sudo apt install python3-twisted
+sudo apt install python3-bitarray
+sudo apt install python3-dev
 git clone https://github.com/lz5pn/HBlink3
-
-sleep 2
-mv /opt/HBlink3/ /opt/backup/
-mv /opt/backup/HBlink3/ /opt/
-mv /opt/backup/HBmonitor/ /opt/
-
-sleep 2
-chmod +x -R /opt/HBmonitor
-
-sleep 2
+sudo mv /opt/HBlink3/ /opt/backup/
+sudo mv /opt/backup/HBlink3/ /opt/
+sudo mv /opt/backup/HBmonitor/ /opt/
 sudo mv /opt/backup/dmr_utils3/ /opt/
 sudo rm -r /opt/backup/
 
-sleep 2
+sleep2
 cd /opt/dmr_utils3
-chmod +x install.sh
-
-sleep 2
-./install.sh
-
-sleep 2
-/usr/bin/python3 -m pip install --upgrade pip
-
-sleep 2
-pip install --upgrade dmr_utils3
+sudo chmod +x install.sh
+sudo ./install.sh
+sudo /usr/bin/python3 -m pip install --upgrade pip
+sudo pip install --upgrade dmr_utils3
 
 sleep 2
 cd /opt/HBlink3
-cp hblink-SAMPLE.cfg hblink.cfg
-
-sleep 2
-cp rules-SAMPLE.py rules.py
+sudo cp hblink-SAMPLE.cfg hblink.cfg
+sudo cp rules-SAMPLE.py rules.py
 
 #Autostart HBLink:
-nano /lib/systemd/system/hblink.service
-#Copiar y pegar el contenido siguiente:
-____________________________________________
-[Unit]
-Description=Start HBlink
-After=multi-user.target
-[Service]
-ExecStart=/usr/bin/python3 /opt/HBlink3/bridge.py
-[Install]
-WantedBy=multi-user.target
-______________________________________________
+sudo cp /home/pi/V106/hblink.service /lib/systemd/system/
 
-sleep 2
 sudo systemctl daemon-reload
 sudo systemctl enable hblink
 
-sleep 2
 #Instalar Parrot para  Echotest:
-chmod +x playback.py
+sudo chmod +x playback.py
 
-sleep 2
 #Crear directorio  /var/log/hblink si no está creado
-mkdir /var/log/hblink
+sudo mkdir /var/log/hblink
 
-sleep 2
 #Crear servicio para el parrot /lib/systemd/system/parrot.service 
-nano /lib/systemd/system/parrot.service
-#Copiar y pegar el contenido siguiente:
-____________________________________________
-[Unit]
-Description=HB bridge all Service
-After=network-online.target syslog.target
-Wants=network-online.target
-[Service]
-StandardOutput=null
-WorkingDirectory=/opt/HBlink3
-RestartSec=3
-ExecStart=/usr/bin/python3 /opt/HBlink3/playback.py -c /opt/HBlink3/playback.cfg
-Restart=on-abort
-[Install]
-WantedBy=multi-user.target
-____________________________________________
+sudo cp /home/pi/V106/parrot.service /lib/systemd/system/
 
-sleep 2
-systemctl enable parrot.service
-systemctl start parrot.service
+sudo systemctl enable parrot.service
+sudo systemctl start parrot.service
 #sudo systemctl status parrot.service
 
-sleep 2
 #Configurar HBlink
-#nano /opt/HBlink3/hblink.cfg
-#nano /opt/HBlink3/rules.py
+#sudo nano /opt/HBlink3/hblink.cfg
+#sudo nano /opt/HBlink3/rules.py
 
-sleep 2
 #Test de configuración:
-python3 /opt/HBlink3/bridge.py
+#python3 /opt/HBlink3/bridge.py
 
 #Si es ok salir con  "ctrl-c" y start los servicios
 
-
+sudo systemctl start hblink
+#sudo systemctl status hblink
 
 sleep 2
 #Instalar web monitor para HBLink.
 cd /opt/HBmonitor
-chmod +x install.sh
+sudo chmod +x install.sh
+sudo ./install.sh
+sudo cp config_SAMPLE.py config.py
+#sudo nano /opt/HBmonitor/config.py
 
-sleep 2
-./install.sh
-
-sleep 2
-cp config_SAMPLE.py config.py
-
-sleep 2
 #Start monitor service:
-cp utils/hbmon.service /lib/systemd/system/
-
-sleep 2
-systemctl enable hbmon
-systemctl start hbmon & systemctl start hblink &
-#sudo systemctl status hblink
-#systemctl status hbmon
+sudo cp utils/hbmon.service /lib/systemd/system/
+sudo systemctl enable hbmon
+sudo systemctl start hbmon
+#sudo systemctl status hbmon
 clear
 echo "********* FIN *****"
 echo "********* FIN *****"
@@ -150,4 +85,4 @@ echo "********** SE REINICIARA EL SISTEMA EN 5 SEGUNDOS ***********"
 echo "********** SE REINICIARA EL SISTEMA EN 5 SEGUNDOS ***********"
 echo "********** SE REINICIARA EL SISTEMA EN 5 SEGUNDOS ***********"
 sleep 5
-reboot
+#reboot
