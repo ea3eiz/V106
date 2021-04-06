9725b17ce0e708dbede448b0ffd06c294c156c90
Manel <ea3eiz@ea3eiz.com>
2021-04-06 23:45:04 +0200

    1

--- editar_rule_uno.sh
+++ editar_rule_uno.sh
@@ -24,14 +24,16 @@ MARRON="\33[38;5;138m"
 						#echo "Indicativo tg conexión y tg desconexión Reflector"
 						echo "Introduce indicativo  ej: EA3EIZ "
 						read ind
+                        echo "Introduce TGID conexión ej: 4373 "
+						read tgid
                         echo "Introduce tg conexión ej: 4373 "
 						read tgc
                         echo "Introduce tg de desconexión ej: 84373"
                         read tgd
-                        echo "Introduce tg de desconexión ej: DMR+4373"
+                        echo "Introduce reflector ej: DMR+4373"
                         read ref
 sudo sed -i "38c '$ref': [ " /opt/HBlink3/rules.py                        
-sudo sed -i "39c {'SYSTEM': '$ind', 'TS': 2, 'TGID': 4373, 'ACTIVE': False, 'TIMEOUT': 10, 'TO_TYPE': 'ON',  'ON': [$tgc], 'OFF': [$tgd], 'RESET': []}," /opt/HBlink3/rules.py
+sudo sed -i "39c {'SYSTEM': '$ind', 'TS': 2, 'TGID': $tgid, 'ACTIVE': False, 'TIMEOUT': 10, 'TO_TYPE': 'ON',  'ON': [$tgc], 'OFF': [$tgd], 'RESET': []}," /opt/HBlink3/rules.py
 sudo sed -i "40c {'SYSTEM': '$ref', 'TS': 2, 'TGID': 9, 'ACTIVE': True, 'TIMEOUT': 2, 'TO_TYPE': 'NONE',  'ON': [], 'OFF': [], 'RESET': []}," /opt/HBlink3/rules.py
 sudo sed -i "213c [$ref] " /opt/HBlink3/hblink.cfg
 
