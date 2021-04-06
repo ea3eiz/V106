9cf9ea62acb168166bfeccb557d2d61fae9bb1ba
Manel <ea3eiz@ea3eiz.com>
2021-04-07 01:08:55 +0200

    1

--- editar_regla_uno.sh
+++ editar_regla_uno.sh
@@ -37,7 +37,39 @@ sudo sed -i "40c ]," /opt/HBlink3/rules.py
 sudo sed -i "41c '$ref': [ " /opt/HBlink3/rules.py                        
 sudo sed -i "42c {'SYSTEM': '$ind', 'TS': 2, 'TGID': $tgid, 'ACTIVE': False, 'TIMEOUT': 10, 'TO_TYPE': 'ON',  'ON': [$tgc], 'OFF': [$tgd], 'RESET': []}," /opt/HBlink3/rules.py
 sudo sed -i "43c {'SYSTEM': '$ref', 'TS': 2, 'TGID': 9, 'ACTIVE': True, 'TIMEOUT': 2, 'TO_TYPE': 'NONE',  'ON': [], 'OFF': [], 'RESET': []}," /opt/HBlink3/rules.py
-sudo sed -i "220c [$ref] " /opt/HBlink3/hblink.cfg
+
+                        sudo sed -i "220c [$ref] " /opt/HBlink3/hblink.cfg 
+                        sudo sed -i "221c MODE: PEER" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "222c ENABLED: True" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "223c LOOSE: True" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "224c EXPORT_AMBE: False" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "225c IP: " /opt/HBlink3/hblink.cfg
+                        sudo sed -i "226c PORT: 54002" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "227c MASTER_IP: 212.237.3.141" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "228c MASTER_PORT: 55555" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "229c PASSPHRASE: ea3eiz-ADER" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "230c CALLSIGN: EA3EIZ" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "231c RADIO_ID: 2143175" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "232c RX_FREQ: 436000000" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "233c TX_FREQ: 436000000" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "234c TX_POWER: 25" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "235c COLORCODE: 1" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "236c SLOTS: 2" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "237c LATITUDE: 41.530704" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "238c LONGITUDE: 2.185437" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "239c HEIGHT: 209" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "240c LOCATION: Barcelona, SPAIN" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "241c DESCRIPTION: This is a cool Hotspot" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "242c URL: www.associacioADER.com" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "243c SOFTWARE_ID: 20191001" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "244c PACKAGE_ID: HBLLINK RASPI" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "245c GROUP_HANGTIME: 5" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "246c OPTIONS: StartRef=4370;RelinkTime=15;" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "247c USE_ACL: True" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "248c REG_ACL: DENY:1" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "249c SUB_ACL: DENY:1" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "250c TGID_TS1_ACL: PERMIT:ALL" /opt/HBlink3/hblink.cfg
+                        sudo sed -i "251c TGID_TS2_ACL: PERMIT:ALL" /opt/HBlink3/hblink.cfg
 
                         break;;
                         [Nn]* ) echo ""
