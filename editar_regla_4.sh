043d731a72d092fa3e2010b7de53a0bb81e0ad91
Manel <ea3eiz@ea3eiz.com>
2021-04-07 18:21:35 +0200

    1

--- editar_regla_4.sh
+++ editar_regla_4.sh
@@ -24,15 +24,15 @@ MARRON="\33[38;5;138m"
 						#echo "Indicativo tg conexión y tg desconexión Reflector"
 						echo "${VERDE}Introduce indicativo  ej: EA3EIZ "
 						read ind
-                        echo "${AMARILLO}Introduce TGID ej: 4373 "
+                        echo "${AMARILLO}Introduce TGID ej: 4374 "
 						read tgid                        
                         echo "${AMARILLO}Introduce TGID por el que quieras salir ej: 214"
 						read tgidsalir
-                        echo "${BLANCO}Introduce tg conexión ej: 4373 "
+                        echo "${BLANCO}Introduce tg conexión ej: 4374 "
 						read tgc
-                        echo "${CIAN}Introduce tg de desconexión ej: 84373"
+                        echo "${CIAN}Introduce tg de desconexión ej: 84374"
                         read tgd
-                        echo "${MARRON}Introduce reflector ej: DMR+4373"
+                        echo "${MARRON}Introduce reflector ej: DMR+4374"
                         read ref
 
 sudo sed -i "60c ]," /opt/HBlink3/rules.py
