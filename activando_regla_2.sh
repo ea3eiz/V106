8c2cab26b6eee3f06e25fa3f79c077dade7943b2
Manel <ea3eiz@ea3eiz.com>
2021-04-09 22:45:26 +0200

    1

--- activando_regla_2.sh
+++ activando_regla_2.sh
@@ -13,7 +13,7 @@ MARRON="\33[38;5;138m"
                         echo "${VERDE}"
                         echo "*****************************************************************************"
                         echo "*****************************************************************************"
-                        echo "                           PARANDO REGLA Y PEER 2                            "
+                        echo "                           ACTIVANDO REGLA Y PEER 2                            "
                         echo "*****************************************************************************"
                         echo "*****************************************************************************"
                         sleep 5
@@ -21,19 +21,19 @@ MARRON="\33[38;5;138m"
                         case $actualizar in
                         [sSyY]* ) echo ""
 
-sudo ded -i "222c ENABLED: False" /opt/HBlink3/hblink.cfg
+sudo ded -i "222c ENABLED: True" /opt/HBlink3/hblink.cfg
 
 line40=$(awk "NR==40" /opt/HBlink3/rules.py)
-sudo sed -i "40c #$line40" /opt/HBlink3/rules.py
+sudo sed -i "40c # $line40" /opt/HBlink3/rules.py
 
 line41=$(awk "NR==41" /opt/HBlink3/rules.py)
-sudo sed -i "41c #$line41" /opt/HBlink3/rules.py
+sudo sed -i "41c # $line41" /opt/HBlink3/rules.py
 
 line42=$(awk "NR==42" /opt/HBlink3/rules.py)
-sudo sed -i "42c #$line42" /opt/HBlink3/rules.py
+sudo sed -i "42c # $line42" /opt/HBlink3/rules.py
 
 line43=$(awk "NR==43" /opt/HBlink3/rules.py)
-sudo sed -i "43c #$line43" /opt/HBlink3/rules.py
+sudo sed -i "43c # $line43" /opt/HBlink3/rules.py
 
                         break;;
                         [Nn]* ) echo ""
