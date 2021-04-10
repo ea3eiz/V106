4e8236a1024176b7d56848ef33c4cad3345a7c5a
Manel <ea3eiz@ea3eiz.com>
2021-04-10 02:06:00 +0200

    1

--- activar_regla_2.sh
+++ activar_regla_2.sh
@@ -25,19 +25,19 @@ sudo sed -i "222c ENABLED: True" /opt/HBlink3/hblink.cfg
 
 line40=$(awk "NR==40" /opt/HBlink3/rules.py)
 line40=${line40#"#"} #borra la primera letra de la variable
-sudo sed -i "40c $aline40" /opt/HBlink3/rules.py
+sudo sed -i "40c $line40" /opt/HBlink3/rules.py
 
 line41=$(awk "NR==41" /opt/HBlink3/rules.py)
 line41=${line41#"#"} #borra la primera letra de la variable
-sudo sed -i "41c $aline41" /opt/HBlink3/rules.py
+sudo sed -i "41c $line41" /opt/HBlink3/rules.py
 
 line42=$(awk "NR==42" /opt/HBlink3/rules.py)
 line42=${line42#"#"} #borra la primera letra de la variable
-sudo sed -i "42c $aline42" /opt/HBlink3/rules.py
+sudo sed -i "42c $line42" /opt/HBlink3/rules.py
 
 line43=$(awk "NR==43" /opt/HBlink3/rules.py)
 line43=${line43#"#"} #borra la primera letra de la variable
-sudo sed -i "43c $aline40" /opt/HBlink3/rules.py
+sudo sed -i "43c $line43" /opt/HBlink3/rules.py
 
                         break;;
                         [Nn]* ) echo ""
