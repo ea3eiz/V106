401c471764c1a121f7572026d35233d161324018
Manel <ea3eiz@ea3eiz.com>
2021-04-10 11:04:26 +0200

    1

--- desactivar_regla2.sh
+++ desactivar_regla2.sh
@@ -24,7 +24,7 @@ MARRON="\33[38;5;138m"
 #ENABLED: False
 
 estado=$(awk "NR==222" /opt/HBlink3/hblink.cfg)
-if [ $estado = "ENABLED: False" ]
+if [ $estado = ENABLED: False ]
 then
 echo "La Regla está activada $estado"
 else
