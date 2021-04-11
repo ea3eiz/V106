6c06b6440c80f646e3038fc8d05a2a222c59f71b
Manel <ea3eiz@ea3eiz.com>
2021-04-12 00:10:13 +0200

    1

--- activar_regla2.sh
+++ activar_regla2.sh
@@ -8,50 +8,33 @@ AMARILLO="\033[1;33m"
 CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
-
                         echo "\v\v\v"
                         echo "${VERDE}"
                         echo "*****************************************************************************"
                         echo "*****************************************************************************"
-                        echo "                           ACTIVANDO REGLA Y PEER 2                            "
+                        echo "                           ACTIVANDO REGLA Y PEER 2                          "
                         echo "*****************************************************************************"
                         echo "*****************************************************************************"
                         sleep 5
-
-# 0 = La Regla NO está creada
-# 1 = La Regla SI está creada
-# 2 = La Regla está ACTIVADA
-# 3 = La Regla está DESACTIVADO
-
-estado=$(awk "NR==22" /home/pi/info.ini)
-
-if [ $estado = 3 ]
-then
-
-sudo sed -i "222c ENABLED: True" /opt/HBlink3/hblink.cfg
-
-line40=$(awk "NR==40" /opt/HBlink3/rules.py)
-line40=${line40#"#"} #borra la primera letra de la variable
-sudo sed -i "40c $line40" /opt/HBlink3/rules.py
-
-line41=$(awk "NR==41" /opt/HBlink3/rules.py)
-line41=${line41#"#"} #borra la primera letra de la variable
-sudo sed -i "41c $line41" /opt/HBlink3/rules.py
-
-line42=$(awk "NR==42" /opt/HBlink3/rules.py)
-line42=${line42#"#"} #borra la primera letra de la variable
-sudo sed -i "42c $line42" /opt/HBlink3/rules.py
-
-line43=$(awk "NR==43" /opt/HBlink3/rules.py)
-line43=${line43#"#"} #borra la primera letra de la variable
-sudo sed -i "43c $line43" /opt/HBlink3/rules.py
-
-sudo systemctl restart hbmon
-sudo systemctl restart hblink
-
-sudo sed -i "22c 2" /home/pi/info.ini
-
-
+                        estado=$(awk "NR==22" /home/pi/info.ini)
+                        if [ $estado = 3 ]
+                        then
+                        sudo sed -i "222c ENABLED: True" /opt/HBlink3/hblink.cfg
+                        line40=$(awk "NR==40" /opt/HBlink3/rules.py)
+                        line40=${line40#"#"} #borra la primera letra de la variable
+                        sudo sed -i "40c $line40" /opt/HBlink3/rules.py
+                        line41=$(awk "NR==41" /opt/HBlink3/rules.py)
+                        line41=${line41#"#"} #borra la primera letra de la variable
+                        sudo sed -i "41c $line41" /opt/HBlink3/rules.py
+                        line42=$(awk "NR==42" /opt/HBlink3/rules.py)
+                        line42=${line42#"#"} #borra la primera letra de la variable
+                        sudo sed -i "42c $line42" /opt/HBlink3/rules.py
+                        line43=$(awk "NR==43" /opt/HBlink3/rules.py)
+                        line43=${line43#"#"} #borra la primera letra de la variable
+                        sudo sed -i "43c $line43" /opt/HBlink3/rules.py
+                        sudo systemctl restart hbmon
+                        sudo systemctl restart hblink
+                        sudo sed -i "22c 2" /home/pi/info.ini
                         clear
                         echo "\v\v\v"
                         echo "${AMARILLO}"
@@ -62,9 +45,8 @@ sudo sed -i "22c 2" /home/pi/info.ini
                         echo "*****************************************************************************"
                         sleep 5
                         exit
-elif [ $estado = 22 ]
-then
-
+                        elif [ $estado = 22 ]
+                        then
                         clear
                         echo "\v\v\v"
                         echo "${ROJO}"
@@ -75,8 +57,7 @@ then
                         echo "*****************************************************************************"
                         sleep 5
                         exit
-else
-
+                        else
                         clear
                         echo "\v\v\v"
                         echo "${ROJO}"
@@ -87,7 +68,5 @@ else
                         echo "*****************************************************************************"
                         sleep 5
                         exit
-
-
-fi
+                        fi
 
