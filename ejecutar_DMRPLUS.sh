ff815b97243dc6b3122ab0a2870efe319140bf40
Manel <ea3eiz@ea3eiz.com>
2024-01-14 12:43:02 +0100

    Update ejecutar_DMRPLUS.sh

--- ejecutar_DMRPLUS.sh
+++ ejecutar_DMRPLUS.sh
@@ -37,7 +37,7 @@ sudo rm /home/pi/Abrir_MMDVMPLUS.desktop
 cd /home/pi/MMDVMHost
 echo "\33[1;37m"
 echo " **************************************************************************"
-echo "                               ABRIENDO DMR+                              "
+echo "                               ABRIENDO DMR+++++++                              "
 echo " **************************************************************************"
 sleep 2
 #/home/pi/V106/./qt_info_dmrplus & sudo ./MMDVMPLUS MMDVMPLUS.ini
