545365a250be8beef101dea45d037cf082201f3e
Manel <ea3eiz@ea3eiz.com>
2024-04-20 16:27:39 +0200

    Update instalar_anydesk.sh
    1

--- instalar_anydesk.sh
+++ instalar_anydesk.sh
@@ -52,5 +52,5 @@ MARRON="\33[38;5;138m"
                         echo "***********************************************************************"                      
                         sed -i "12c ANYDESK=ON" /home/pi/info.ini                        
                         sleep 3
-                        sudo reboot
+                        #sudo reboot
                         fi
