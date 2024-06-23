cdfc4458793d7991fd8a96604fc0a2d828eb36f3
Manel <ea3eiz@ea3eiz.com>
2024-06-24 01:22:28 +0200

    Update ejecutar_solofusion.sh

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -42,7 +42,7 @@ sudo rm /home/pi/Abrir_solofusion.desktop
 cd /home/pi/YSFClients/YSFGateway
 #sudo lxterminal --geometry=77x12 -e ./YSFGateway YSFGateway.ini & 
 
-xterm -geometry 88x3+1270+700 -bg black -fg brown -fa â€verdanaâ€ -fs 9x -T YSFGateway -e ./YSFGateway YSFGateway.ini &
+xterm -geometry 88x3+1270+675 -bg black -fg brown -fa â€verdanaâ€ -fs 9x -T YSFGateway -e ./YSFGateway YSFGateway.ini &
 
 cd /home/pi/MMDVMHost
 #/home/pi/V106/./qt_info_solofusion & sudo ./MMDVMFUSION MMDVMFUSION.ini
