c3aa17d4ebf9a43341335552c9b342595ae71c39
Manel <ea3eiz@ea3eiz.com>
2024-07-22 11:46:55 +0200

    Update ejecutar_svxlink.sh

--- ejecutar_svxlink.sh
+++ ejecutar_svxlink.sh
@@ -21,20 +21,11 @@ sudo cp Abrir_svxlink.desktop /home/pi/Desktop
 
 sudo rm /home/pi/Abrir_svxlink.desktop
 
-echo "${BLANCO}"
-echo " ************************************************"
-echo "               ABRIENDO SVXLINK                  "
-echo " ************************************************"
-sleep 2
-
 xterm -geometry 88x14+1271+22 -bg blue -fg white -fa â€verdanaâ€ -fs 9x -T SVXLINK -e sudo svxlink
-
-
-sudo svxlink
 
 cd /home/pi/Desktop
 sudo cp Abrir_svxlink.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=50x28 --title=SVXLINK -e sudo sh ejecutar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh ejecutar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_SVXLINK_OFF.png" /home/pi/Abrir_svxlink.desktop
 sed -i "10c Name[es_ES]=Abrir SVXLINK" /home/pi/Abrir_svxlink.desktop
 sed -i "8c SVXLINK=OFF" /home/pi/status.ini
