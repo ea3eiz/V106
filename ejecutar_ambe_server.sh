ed92f16bdcf97914fc4b79d6b200676fe0b01f25
Manel <ea3eiz@ea3eiz.com>
2024-07-25 19:36:23 +0200

    1

--- ejecutar_ambe_server.sh
+++ ejecutar_ambe_server.sh
@@ -26,7 +26,7 @@ xterm -geometry 88x6+1122+49 -bg black -fg white -fa â€verdanaâ€ -fs 9x -T
 
 cd /home/pi/Desktop
 sudo cp Abrir_ambe_server.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_ambe_server.sh'" /home/pi/Abrir_ambe_server.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh ejecutar_ambe_server.sh'" /home/pi/Abrir_ambe_server.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AMBE_SERVER_OFF.png" /home/pi/Abrir_ambe_server.desktop
 sed -i "10c Name[es_ES]=Abrir AMBE SERVER" /home/pi/Abrir_ambe_server.desktop
 sed -i "11c AMBE_SERVER=OFF" /home/pi/status.ini
