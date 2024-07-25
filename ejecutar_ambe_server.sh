73d6b892333f14a3f2e8c91a86977553ef818a13
Manel <ea3eiz@ea3eiz.com>
2024-07-25 19:50:02 +0200

    1

--- ejecutar_ambe_server.sh
+++ ejecutar_ambe_server.sh
@@ -20,13 +20,12 @@ cd /home/pi/AMBE_SERVER
 
 sudo killall AMBEserver
 sleep 1
-xterm -geometry 88x6+1122+49 -bg black -fg white -fa â€verdanaâ€ -fs 9x -T CONSLOLA_AMBEserber -e ./AMBEserver -p $puerto_router -i $puerto_modem -s $baut_rate
-
+./AMBEserver -p $puerto_router -i $puerto_modem -s $baut_rate
 
 
 cd /home/pi/Desktop
 sudo cp Abrir_ambe_server.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh ejecutar_ambe_server.sh'" /home/pi/Abrir_ambe_server.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_ambe_server.sh'" /home/pi/Abrir_ambe_server.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AMBE_SERVER_OFF.png" /home/pi/Abrir_ambe_server.desktop
 sed -i "10c Name[es_ES]=Abrir AMBE SERVER" /home/pi/Abrir_ambe_server.desktop
 sed -i "11c AMBE_SERVER=OFF" /home/pi/status.ini
