c2dff1a7e6da4abf5b10914462a5ee4874f2dd6f
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-07-28 18:38:11 +0200

    Update actualizar_YSF.sh

--- actualizar_YSF.sh
+++ actualizar_YSF.sh
@@ -1,5 +1,4 @@
 ﻿#!/bin/bash
-clear
 #Colores
 ROJO="\033[1;31m"
 VERDE="\033[1;32m"
@@ -9,38 +8,14 @@ CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
 
-# path usuario
-usuario="/home/pi"
-usuario="$usuario"
-						
-						echo "\v\v\v\v\v"
-						echo "${VERDE}"
-						echo "   **************************************************************************"
-						echo "   **************************************************************************"
-						echo "${VERDE}"
-						echo "                   ACTUALIZANDO LISTADO DE SALAS YSF y FCS       "
-						echo "                                                                             "
-						echo -n "${VERDE}"
-						echo "   **************************************************************************"
-						echo "   **************************************************************************"
-						sleep 3
-			            echo "${VERDE}"						
-						cd $usuario/YSFClients/YSFGateway
-						sudo chmod 777 -R $usuario/YSFClients/YSFGateway
-						sudo wget -O YSFHosts.txt http://register.ysfreflector.de/export_csv.php					
-						wget -O FCSRooms.txt https://raw.githubusercontent.com/g4klx/YSFClients/master/YSFGateway/FCSRooms.txt 
-						sleep 3
-						clear
-						echo "\v\v\v\v\v"
-						echo "${BLANCO}"
-						echo "   **************************************************************************"
-						echo "   **************************************************************************"
-						echo "${VERDE}"
-						echo "             Los fichero YSFHost.txt y FCSRooms.txt se han actualizado       "
-						echo "                                                                             "
-						echo -n "${BLANCO}"
-						echo "   **************************************************************************"
-						echo "   **************************************************************************"
-						sleep 3
-						exit;
-						
+            cd /home/pi/YSFClients/YSFGateway
+			
+            curl https://dvref.com/downloads/YSFHosts-resolved.txt > home/pi/YSFClients/YSFGateway/YSFHosts.txt
+			sleep 3
+
+			sed -i '$a\\21887;ANDALUCIA;ANDALUCIA;51.68.71.60;42000;022' home/pi/YSFClients/YSFGateway/YSFHosts.txt
+			clear
+			echo "${VERDE}*********************************"
+					echo "* ACTUALIZANDO REFLECTORES YSF  *"
+					echo "*********************************"
+			sleep 3
