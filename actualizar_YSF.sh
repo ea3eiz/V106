3d7c58d2eafc49a39f69f343bcdd56c255cda69a
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-10-27 11:17:03 +0100

    Update actualizar_YSF.sh

--- actualizar_YSF.sh
+++ actualizar_YSF.sh
@@ -8,14 +8,8 @@ CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
 
-            cd /home/pi/YSFClients/YSFGateway
-			
-            curl https://dvref.com/downloads/YSFHosts-resolved.txt > /home/pi/YSFClients/YSFGateway/YSFHosts.txt
-			sleep 3
-
-			sed -i '$a\\21887;ANDALUCIA;ANDALUCIA;51.68.71.60;42000;022' /home/pi/YSFClients/YSFGateway/YSFHosts.txt
-			clear
+            cp /home/pi/V106/YSFHosts.txt /home/pi/YSFClients/YSFGateway/clear
 			echo "${VERDE}*********************************"
-					echo "* ACTUALIZANDO REFLECTORES YSF  *"
+					echo "* ACTUALIZANDO REFLECTORES YSFHosts.txt  *"
 					echo "*********************************"
 			sleep 3
