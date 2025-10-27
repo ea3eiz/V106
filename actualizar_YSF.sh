b0b2e6175913fb4a4f827286626425bf89af0f94
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-10-27 11:26:59 +0100

    Update actualizar_YSF.sh

--- actualizar_YSF.sh
+++ actualizar_YSF.sh
@@ -1,4 +1,5 @@
 ﻿#!/bin/bash
+clear
 #Colores
 ROJO="\033[1;31m"
 VERDE="\033[1;32m"
@@ -8,7 +9,8 @@ CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
 
-            cp /home/pi/V106/YSFHosts.txt /home/pi/YSFClients/YSFGateway/
+            
+			cp /home/pi/V106/YSFHosts.txt /home/pi/YSFClients/YSFGateway/
 			
 			echo "${VERDE}******************************************"
 					echo "* ACTUALIZANDO REFLECTORES YSFHosts.txt  *"
